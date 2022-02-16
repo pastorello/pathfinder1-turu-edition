028d73ffbfa8335dba3437109cdeeada6dfac174
pastor3llo <luca.pastorello@buzzoole.com>
2022-02-16 16:44:46 +0100

    appunti

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 const Home = (props) => {
   return (
-    <h1>
-      Pathfinder 2 <small>Turu's Edition</small>
-    </h1>
+    <>
+      <h1>
+        Pathfinder 2 <small>Turu's Edition</small>
+      </h1>
+      <ul>
+        <li>
+          https://golarion.altervista.org/wiki/Condizioni, ricopiare condizioni
+        </li>
+        <li>creare spellbook per PF1 con classi/scuole personalizzate</li>
+      </ul>
+    </>
   );
 };
 
