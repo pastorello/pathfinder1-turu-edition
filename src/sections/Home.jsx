a2acafbc0e7564b56d04ecb0cc98a65268c9bef9
pastorello <luca@pastorello.info>
2022-02-17 00:12:09 +0100

    refixing home

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -4,14 +4,8 @@ const Home = (props) => {
   return (
     <>
       <h1>
-        Pathfinder 2 <small>Turu's Edition maronn</small>
+        Pathfinder 1 <small>Turu's Edition</small>
       </h1>
-      <ul>
-        <li>
-          https://golarion.altervista.org/wiki/Condizioni, ricopiare condizioni
-        </li>
-        <li>creare spellbook per PF1 con classi/scuole personalizzate</li>
-      </ul>
     </>
   );
 };
